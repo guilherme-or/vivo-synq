b8e2459984f8958161bd2bb75a667b31a9409360
guilherme-or <guioliroc@gmail.com>
2024-09-12 21:54:00 -0300

    feat(api-entity): product timezone fix: number to correct datetime format

--- gateway/internal/entity/product.go
+++ gateway/internal/entity/product.go
@@ -1,5 +1,7 @@
 package entity
 
+import "time"
+
 // Product representa um produto contratado pelo usuário
 // Esse produto é preparado para a resposta da API
 type Product struct {
@@ -8,8 +10,8 @@ type Product struct {
 	ProductName      string         `json:"product_name" bson:"product_name"`
 	ProductType      string         `json:"product_type" bson:"product_type"`
 	SubscriptionType string         `json:"subscription_type" bson:"subscription_type"`
-	StartDate        int64          `json:"start_date" bson:"start_date"`
-	EndDate          int64          `json:"end_date,omitempty" bson:"end_date"`
+	StartDate        time.Time      `json:"start_date" bson:"start_date"`
+	EndDate          time.Time      `json:"end_date,omitempty" bson:"end_date"`
 	UserID           int            `json:"-" bson:"user_id"`
 	ParentProductID  *int           `json:"-" bson:"parent_product_id"`
 	SubProducts      *[]Product     `json:"sub_products,omitempty" bson:"sub_products"`
